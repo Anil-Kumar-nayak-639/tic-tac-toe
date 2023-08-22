509132889d83d06775687f01fbd6338f65a43e80
Anil kumar nayak <anilkumarnayak639@gmail.com>
2023-08-23 00:00:22 +0530

    testing git

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import TicTacToe from './component/TicTacToe';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TicTacToe from "./component/TicTacToe"; //testing git
 function App() {
   return (
     <div className="container">
